edbe51559378a3690b1e646601b10ec92b26fba0
piyushchhabra96 <piyush.chhabra96@gmail.com>
2017-05-20 00:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,19 +39,15 @@ def makeWebhookResult(req):
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
     speech="Search results:"
     #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-    #URL="http://library.thapar.edu/cgi-bin/koha/opac-search.pl?idx=&q=data+structure"
-    #r = requests.get(URL)
+    URL="http://www.google.co.in"
+    r = requests.get(URL)
 
-    #soup = BeautifulSoup(r.content)
+    soup = BeautifulSoup(r.content)
 
-    #links = soup.find_all("a",{"class","title"})
     
-    #for items in links:
-     #   speech=speech+items.text
+    
         
-    speech="Hello Mangal"
-
-    
+    speech=soup    
     print("Response:")
     print(speech)
 
