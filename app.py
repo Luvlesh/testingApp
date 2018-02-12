532f7eeb6f834e2805c3407161cead308897e72f
Luvlesh GARG <luvlesh.garg@amadeus.com>
2018-02-12 12:12:19 +0530

    correted the indentation

--- app.py
+++ app.py
@@ -14,18 +14,18 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+	req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+	print("Request:")
+	print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+	res = makeWebhookResult(req)
 
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+	res = json.dumps(res, indent=4)
+	print(res)
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 def makeWebhookResult(req):
 	print(req.get("result").get("action"))
